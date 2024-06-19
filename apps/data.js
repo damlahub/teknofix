8b597759581c5e309285591f2a1e41eb027e58af
Damla Oksel <damlaoksel@hotmail.com>
2024-06-19 22:34:13 +0300

    teknofix warehouse is completed

---
+++ apps/data.js
@@ -0,0 +1,27 @@
+const DATA=[
+    {
+        id:0,
+        projectName: "V10",
+        pieces:0
+    },
+    {
+        id:1,
+        projectName: "V11",
+        pieces:0
+    },
+    {
+        id:2,
+        projectName: "Garanti Abacus",
+        pieces:0
+    },
+    {
+        id:3,
+        projectName: "INVIA",
+        pieces:0
+    },
+    {
+        id:4,
+        projectName: "AKBANK",
+        pieces:0
+    }
+];
---
+++ apps/projects.js
@@ -0,0 +1,51 @@
+const _main = document.querySelector("main");
+const projects = document.querySelector("#projects");
+const totalElement = document.querySelector("#total");
+
+const loadText = (id) => {
+    const savedText = localStorage.getItem(`project_${id}_text`);
+    return savedText ? savedText : 0;
+};
+
+const saveText = (id, text) => {
+    localStorage.setItem(`project_${id}_text`, text);
+};
+
+const updateTotal = () => {
+    let total = 0;
+    DATA.forEach(project => {
+        total += parseInt(loadText(project.id)) ;
+    });
+    totalElement.textContent = `TOTAL : ${total}`;
+};
+
+const createProject = (project) => {
+    const { id, projectName } = project;
+    let text = loadText(id);
+
+    let projectItem = document.createElement("article");
+    projectItem.classList.add("project");
+
+    projectItem.innerHTML = `
+        <h1>${projectName}</h1>
+        <h3><input type="text" class="projectText" value="${text}" /></h3>
+        <h3>ADET</h3>
+    `;
+
+    const inputElem = projectItem.querySelector(".projectText");
+
+    inputElem.addEventListener("input", () => {
+        saveText(id, inputElem.value);
+        updateTotal();
+    });
+
+    return projectItem;
+};
+
+
+
+DATA.forEach((project) => {
+    projects.appendChild(createProject(project));
+});
+
+updateTotal();
