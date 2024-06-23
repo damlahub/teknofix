f609c5101c19ce3d225c8da4598450d3eb02ab6c
Damla Oksel <damlaoksel@hotmail.com>
2024-06-23 12:02:05 +0300

    Title part is completed.

---
+++ apps/title.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var buttons = document.querySelectorAll('button');
+    console.log(buttons);
+    buttons.forEach((button)=> {
+        button.addEventListener('click', function() {
+            let buttonID = this.id;
+            console.log(buttonID);
+            let title = document.querySelector(".title");
+            title.innerHTML =buttonID;
+        });
+    });
+});
