94e8af4509009f0b5a4e39592987a2b230a94ce3
Damla Oksel <damlaoksel@hotmail.com>
2024-06-23 11:47:51 +0300

    Hostnames part is completed.

--- apps/data.js
+++ apps/data.js
@@ -46,3 +46,35 @@ const pdfDATA=[
         path:"test3"
     },
 ];
+const hostnamesDATA=[
+    {
+        id:0,
+        projectName:"ProjectName",
+        hostname:"HOSTNAME"
+    },
+    {
+        id:1,
+        projectName:"ProjectName1",
+        hostname:"HOSTNAME1"
+    },
+    {
+        id:2,
+        projectName:"ProjectName2",
+        hostname:"HOSTNAME2"
+    },
+    {
+        id:3,
+        projectName:"ProjectName3",
+        hostname:"HOSTNAME3"
+    },
+    {
+        id:4,
+        projectName:"ProjectName4",
+        hostname:"HOSTNAME4"
+    },
+    {
+        id:5,
+        projectName:"ProjectName5",
+        hostname:"HOSTNAME5"
+    },
+]
---
+++ apps/hostnames.js
@@ -0,0 +1,37 @@
+{/* <table style="width:100%">
+
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+  </tr>
+</table> */}
+const hostnamesBTN = document.querySelector("#hostnames");
+let hostTable= document.createElement("table");
+
+const createHostname = (hostnames) => {
+    const { id, projectName, hostname } = hostnames;
+    let hostnameRow= document.createElement("tr");
+    hostnameRow.id=id;
+    hostnameRow.innerHTML=`
+        <td>${projectName}</td>
+        <td>${hostname}</td>
+    `;
+    if(id%2==0){
+        hostnameRow.style.backgroundColor="rgb(255, 0, 106)";
+    }
+    hostTable.appendChild(hostnameRow); 
+    return hostnameRow;
+}
+hostnamesBTN.addEventListener("click", () => {
+    _main.innerHTML = "";
+    _main.className="";
+    _main.classList.add("hostnameLayout");
+    _main.appendChild(hostTable);
+    hostnamesDATA.forEach((hostname) => {
+        createHostname(hostname);
+    });
+});
